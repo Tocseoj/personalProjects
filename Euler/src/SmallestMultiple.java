06343a4ea3ab2451ccf9dcde4694279f19cd90c1
Joseph Marshall <marsha87@purude.edu>
2015-11-09 13:27:01 -0500

    Euler problem 5

---
+++ Euler/src/SmallestMultiple.java
@@ -0,0 +1,21 @@
+/**
+ * CS180 - Euler: SmallestMultiple
+ * Find the smallest
+ * @author Joseph Marshall, marsha87@purdue.edu, 804
+ */
+
+public class SmallestMultiple {
+    public static void main(String[] args) {
+        int sm = 0;
+        boolean smallestMultiple = false;
+        while (!smallestMultiple) {
+            sm++;
+            smallestMultiple = true;
+            for (int i = 1; i <= 20; i++) {
+                if (sm % i != 0)
+                    smallestMultiple = false;
+            }
+        }
+        System.out.println(sm);
+    }
+}
